51eb4c179731ec4486a52e294332a39d730c7acc
yuanchengyong <zyycy_love@126.com>
2020-05-07 22:41:05 +0800

    change to ts

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,15 @@
 /*
- * @Author: yuanchengyong 
- * @Date: 2020-02-03 12:47:40 
+ * @Author: yuanchengyong
+ * @Date: 2020-02-03 12:47:40
  * @Last Modified by: yuanchengyong
- * @Last Modified time: 2020-02-03 12:48:30
+ * @Last Modified time: 2020-05-07 22:28:46
  * @Des: 开发代理
  */
-const proxy = require('http-proxy-middleware');
-
+const {
+    createProxyMiddleware
+} = require("http-proxy-middleware");
 module.exports = function (app) {
-    app.use(proxy('/api', {
+    app.use(createProxyMiddleware('/api', {
         target: 'http://localhost:3001/'
     }));
 };
